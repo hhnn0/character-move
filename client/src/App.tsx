4d3bcda25d26f5048b2f3bebf549d92342c95a94
hhnn0 <hyun0520ho@gmail.com>
2023-02-11 03:38:16 +0900

    chore: 초기 셋팅

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import GlobalStyle from "./styles/GlobalStyle";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <div className="App">asd</div>
+    </>
+  );
+}
+
+export default App;
